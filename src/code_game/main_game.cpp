7954707efd80343e09b812b8f07df6c56918b37a
ganovelli <fabio.ganovelli@gmail.com>
2024-02-09 17:57:36 +0100

    ongoing drawing of game

--- src/code_game/main_game.cpp
+++ src/code_game/main_game.cpp
@@ -9,77 +9,175 @@
 #include "..\common\game.h"
 #include "..\common\game_builder.h"
 
-int main(void)
+#include <iostream>
+#include <algorithm>
+#include <conio.h>
+#include <direct.h>
+#include "..\common\matrix_stack.h"
+#include "..\common\intersection.h"
+#include "..\common\trackball.h"
+
+trackball tb[2];
+int curr_tb;
+
+/* projection matrix*/
+glm::mat4 proj;
+
+/* view matrix */
+glm::mat4 view;
+
+matrix_stack stack;
+float scaling_factor = 1.0;
+
+
+/* callback function called when the mouse is moving */
+static void cursor_position_callback(GLFWwindow * window, double xpos, double ypos)
 {
+	tb[curr_tb].mouse_move(proj, view, xpos, ypos);
+}
 
-	race r;
-	game_builder::default_race(r);
-
-	r.start();
-	while (true) {
-		r.update();
-
-		std::cout << glm::to_string(r.cars[0].frame) << std::endl;
-
+/* callback function called when a mouse button is pressed */
+void mouse_button_callback(GLFWwindow * window, int button, int action, int mods)
+{
+	if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		tb[curr_tb].mouse_press(proj, view, xpos, ypos);
 	}
- //   GLFWwindow* window;
+	else
+		if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
+			tb[curr_tb].mouse_release();
+		}
+}
 
- //   /* Initialize the library */
- //   if (!glfwInit())
- //       return -1;
+/* callback function called when a mouse wheel is rotated */
+void scroll_callback(GLFWwindow * window, double xoffset, double yoffset)
+{
+	if (curr_tb == 0)
+		tb[0].mouse_scroll(xoffset, yoffset);
+}
 
- //   /* Create a windowed mode window and its OpenGL context */
- //   window = glfwCreateWindow(1000, 800, "indexed", NULL, NULL);
- //   if (!window)
- //   {
- //       glfwTerminate();
- //       return -1;
- //   }
-
- //   
- //   /* Make the window's context current */
- //   glfwMakeContextCurrent(window);
-
- //   glewInit();
-
- //   printout_opengl_glsl_info();
-
- //   /* define a cube */
- //   shape cube;
- //   shape_maker::cube(cube, 0.0f, 1.0f, 0.0f);
-
- //   renderable r;
- //   cube.to_renderable(r);
-
- //   shader basic_shader;
- //   basic_shader.create_program("shaders/basic.vert", "shaders/basic.frag");
- //  
-	///* use the program shader "program_shader" */
-	//glUseProgram(basic_shader.program);
-
- //   /* use the array and element buffers */
- //   r.bind();
-
- //   glClearColor(1, 0, 0,1);
- //   /* Loop until the user closes the window */
- //   while (!glfwWindowShouldClose(window))
- //   {
- //       /* Render here */
- //       glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
- //       //glDrawArrays(GL_TRIANGLES, 0, 3);
- //       glDrawElements(r().mode, r().count, r().itype, NULL);
- //       check_gl_errors(__LINE__,__FILE__);
- //
- //       /* Swap front and back buffers */
- //       glfwSwapBuffers(window);
-
- //       /* Poll for and process events */
- //       glfwPollEvents();
- //   }
-	//glUseProgram(0);
-
- //   glfwTerminate();
- //   return 0;
+void key_callback(GLFWwindow * window, int key, int scancode, int action, int mods) {
+	/* every time any key is presse it switch from controlling trackball tb[0] to tb[1] and viceversa */
+	if (action == GLFW_PRESS)
+		curr_tb = 1 - curr_tb;
 
 }
+
+
+
+	int main(int argc, char** argv)
+	{
+		race r;
+		game_builder::default_race(r);
+
+		//r.start();
+		//while (true) {
+		//	r.update();
+		//	std::cout << glm::to_string(r.cars[0].frame) << std::endl;
+		//}
+
+
+		GLFWwindow* window;
+
+		/* Initialize the library */
+		if (!glfwInit())
+			return -1;
+
+		//	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+		//	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+			/* Create a windowed mode window and its OpenGL context */
+		window = glfwCreateWindow(800, 800, "game", NULL, NULL);
+		if (!window)
+		{
+			glfwTerminate();
+			return -1;
+		}
+		/* declare the callback functions on mouse events */
+		if (glfwRawMouseMotionSupported())
+			glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+
+		glfwSetCursorPosCallback(window, cursor_position_callback);
+		glfwSetMouseButtonCallback(window, mouse_button_callback);
+		glfwSetScrollCallback(window, scroll_callback);
+		glfwSetKeyCallback(window, key_callback);
+
+		/* Make the window's context current */
+		glfwMakeContextCurrent(window);
+
+		glewInit();
+
+		printout_opengl_glsl_info();
+
+		renderable fr = shape_maker::cube();
+
+		shader basic_shader;
+		basic_shader.create_program("shaders/basic.vert", "shaders/basic.frag");
+
+		/* use the program shader "program_shader" */
+		glUseProgram(basic_shader.program);
+
+
+		check_gl_errors(__LINE__, __FILE__);
+		glUseProgram(basic_shader.program);
+
+		check_gl_errors(__LINE__, __FILE__);
+
+
+		check_gl_errors(__LINE__, __FILE__);
+		/* load the shaders */
+		
+		/* define the viewport  */
+		glViewport(0, 0, 800, 800);
+
+		/* avoid rendering back faces */
+		// uncomment to see the plane disappear when rotating it
+		glDisable(GL_CULL_FACE);
+
+		tb[0].reset();
+		tb[0].set_center_radius(glm::vec3(0, 0, 0), 500.f);
+		curr_tb = 0;
+
+		proj = glm::frustum(-10.f, 10.f, -10.f, 10.f, 1.f, 200.f);
+		view = glm::lookAt(glm::vec3(0, 30.f, 40.f), glm::vec3(0.f, 0.f, 0.f), glm::vec3(0.f, 1.f, 0.f));
+		 
+		check_gl_errors(__LINE__, __FILE__);
+		fr.bind();
+		check_gl_errors(__LINE__, __FILE__);
+		 r.start();
+		 r.update();
+		/* Loop until the user closes the window */
+		while (!glfwWindowShouldClose(window))
+		{
+			/* Render here */
+			glClearColor(0.0, 0.0, 0.0, 1.0);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+			check_gl_errors(__LINE__, __FILE__);
+
+			//r.update();
+
+			glUniformMatrix4fv(basic_shader["uP"], 1, GL_FALSE, &proj[0][0]);
+			glUniformMatrix4fv(basic_shader["uV"], 1, GL_FALSE, &view[0][0]);
+	 		glUniformMatrix4fv(basic_shader["uT"], 1, GL_FALSE, &tb[0].matrix()[0][0]);
+			glUniformMatrix4fv(basic_shader["uM"], 1, GL_FALSE, &r.cars[0].frame[0][0]);
+			glDrawElements(fr().mode, fr().count, fr().itype, 0);
+
+	
+			glUniformMatrix4fv(basic_shader["uT"], 1, GL_FALSE, &glm::mat4(1.f)[0][0]);
+			glUniformMatrix4fv(basic_shader["uM"], 1, GL_FALSE, &glm::mat4(1.f)[0][0]);
+			glDrawElements(fr().mode, fr().count, fr().itype, 0);
+
+			/* Swap front and back buffers */
+			glfwSwapBuffers(window);
+
+			/* Poll for and process events */
+			glfwPollEvents();
+		}
+		glUseProgram(0);
+		glfwTerminate();
+		return 0;
+	}
+
+ 
--- src/code_game/shaders/basic.vert
+++ src/code_game/shaders/basic.vert
@@ -1,10 +1,16 @@
 #version 460 core 
-layout (location = 0) in vec2 aPosition; 
+layout (location = 0) in vec3 aPosition; 
 layout (location = 1) in vec3 aColor; 
 
-out vec3 vColor; 
+out vec3 vColor;
+
+uniform mat4 uP;
+uniform mat4 uV;
+uniform mat4 uT;
+uniform mat4 uM;
+
 void main(void) 
 { 
-    gl_Position = vec4(aPosition,0.0, 1.0); 
-    vColor = aColor; 
+	vColor = aColor;
+    gl_Position = uP*uV*uT*uM*vec4(aPosition, 1.0); 
 }
